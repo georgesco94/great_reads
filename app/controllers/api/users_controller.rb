158536e2116747859daea01c2389487ecc19984c
George Najm <georgenajm28@yahoo.com>
2017-10-24 16:51:15 -0400

    prep heroku push

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,6 @@ class Api::UsersController < ApplicationController
 
     if @user.save
       login(@user)
-      debugger
       render 'api/users/show'
     else
       render json: @user.errors.full_messages, status: 422
