75b7a779c94fd5f8036984e4ff48033e0ac1f6b9
George Najm <georgenajm28@yahoo.com>
2017-10-25 09:26:51 -0400

    added errors and session reducers

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import SessionErrorsReducer from './session_errors_reducer';
+
+export default combineReducers(
+  {
+    session:SessionErrorsReducer
+  }
+);
+
+export default ErrorsReducer;
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import merge from 'lodash/merge';
+
+import {RECEIVE_CURRENT_USE} from '../actions/session_actions';
+
+
+
+const SessionReducer = (state = {currentUser: null}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, { currentUser: action.currentUser });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
