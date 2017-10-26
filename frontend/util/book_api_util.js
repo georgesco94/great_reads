79bc17442711ebfe91efdad58a100f84d4860bf2
George Najm <georgenajm28@yahoo.com>
2017-10-26 15:21:13 -0400

    added book api util ajax calls

---
+++ frontend/util/book_api_util.js
@@ -0,0 +1,17 @@
+export const fetchBooks = () => (
+  $.ajax(
+    {
+      method: "GET",
+      url: "api/books",
+    }
+  )
+);
+
+export const fetchBook = (id) => (
+  $.ajax(
+    {
+      method: "GET",
+      url: `api/books/${id}`,
+    }
+  )
+);
