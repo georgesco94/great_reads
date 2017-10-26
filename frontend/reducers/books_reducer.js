fe7452f0af9554560e6f9fdfbe22bfc468886529
George Najm <georgenajm28@yahoo.com>
2017-10-26 16:07:30 -0400

    added entities and books reducer

---
+++ frontend/reducers/books_reducer.js
@@ -0,0 +1,19 @@
+import merge from 'lodash/merge';
+
+import {RECEIVE_BOOKS,RECEIVE_BOOK} from '../actions/book_actions';
+
+
+
+const BooksReducer = (state = {books:{}} , action) => {
+  switch(action.type) {
+    case RECEIVE_BOOKS:
+      return action.books;
+    case RECEIVE_BOOK:
+      return merge({}, state, {[action.book.id]: action.book});
+    default:
+      return state;
+  }
+};
+
+
+export default BooksReducer;
---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import BooksReducer from './books_reducer';
+
+
+export default combineReducers(
+  {
+    books: BooksReducer
+  }
+)
