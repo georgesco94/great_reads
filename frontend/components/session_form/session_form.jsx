8285aaace7c23d54851ce615a7dba6a8189ab0d0
George Najm <georgenajm28@yahoo.com>
2017-10-26 10:59:30 -0400

    added timed out errors display

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -7,15 +7,22 @@ class SessionForm extends React.Component{
     this.state = {
       username: '',
       password: '',
-      email: ''
+      email: '',
+      displayErrors:  this.props.errors.length ? true : false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+    this.removeErrors = this.removeErrors.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const user = this.state;
     this.props.signup(user);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({displayErrors: nextProps.errors.length ? true : false})
   }
 
   update(field){
@@ -25,16 +32,35 @@ class SessionForm extends React.Component{
   }
 
   renderErrors(){
+    const errs = this.props.errors.map((error) => {
+      return <li className="errors">{error}</li>
+    });
+    return (
 
+      <nav className="signup-errors-box">
+        <ul className="errors-ul">
+          {errs}
+        </ul>
+      </nav>
+    )
+  }
+
+  removeErrors(){
+    setTimeout(function() {
+       this.setState({displayErrors: false});
+     }.bind(this), 3000);
   }
 
   render(){
+    if(this.state.displayErrors) {
+      this.removeErrors();
+    }
     return (
       <div className="signup-form-container">
+        {this.state.displayErrors ? this.renderErrors() : ""}
         <div id="login-headling">
           <img alt="Meet your next favorite book." src="https://s.gr-assets.com/assets/home/headline-e2cd420e71737ff2815d09af5b65c4e4.png" />
         </div>
-        {this.renderErrors()}
         <form className="signup-form-box" onSubmit={this.handleSubmit}>
           <h2>New Here? Create a free account!</h2>
           <input className="input-signup" placeholder="Username" type="text" onChange={this.update('username')} />
