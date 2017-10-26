ae9016e7d1a3d6b71a84b4abee50881e6740102f
George Najm <georgenajm28@yahoo.com>
2017-10-26 01:19:58 -0400

    added errors rendering on invalid login/signup

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -12,8 +12,6 @@ class SessionForm extends React.Component{
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
-
   handleSubmit(e) {
     e.preventDefault();
     const user = this.state;
@@ -32,7 +30,7 @@ class SessionForm extends React.Component{
         <div id="login-headling">
           <img alt="Meet your next favorite book." src="https://s.gr-assets.com/assets/home/headline-e2cd420e71737ff2815d09af5b65c4e4.png" />
         </div>
-
+        {this.props.errors}
         <form className="signup-form-box" onSubmit={this.handleSubmit}>
           <h2>New Here? Create a free account!</h2>
           <input className="input-signup" placeholder="Username" type="text" onChange={this.update('username')} />
