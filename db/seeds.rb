07461bc095c29a1163df03c75c30ed5eee1d8f94
George Najm <georgenajm28@yahoo.com>
2017-10-27 17:17:10 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Book.destroy_all
+
 guest = User.create!(username:"guest", email: "guestEmail", password:"123456")
 
-wp_img =
-war_peace = Book.create!(title:"War and Peace", author: "Lev Tolstoy" ,
+war_peace = Book.new(title:"War and Peace", author: "Lev Tolstoy" ,
 description:"The novel chronicles the history of the French invasion of Russia and
 the impact of the Napoleonic era on Tsarist society through the stories of five
 Russian aristocratic families")
 war_peace.image = File.open("app/assets/images/war_peace.jpeg")
+war_peace.save!
 
-stories = Book.create!(title:"The Afterlife", author: "John Updike" ,
+stories = Book.new(title:"The Afterlife", author: "John Updike" ,
 description:"Short stories by John Updike")
-war_peace.image = File.open("app/assets/images/updike_short.jpg")
+stories.image = File.open("app/assets/images/updike_short.jpg")
+stories.save!
 
-snows = Book.create!(title:"Snows of Kilimanjaro", author: "Ernest Hemingway" ,
+snows = Book.new(title:"Snows of Kilimanjaro", author: "Ernest Hemingway" ,
 description:"Collection of short stories by Hemingway")
-war_peace.image = File.open("app/assets/images/snows.jpg")
+snows.image = File.open("app/assets/images/snows.jpg")
+snows.save!
