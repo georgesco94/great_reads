226a2fc453f55233eb5eefbf4ea0cedaa4a2b7d8
George Najm <georgenajm28@yahoo.com>
2017-10-31 17:45:26 -0400

    added reviews jbuilder

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,15 +1,26 @@
 class Api::ReviewsController < ApplicationController
 
   def index
+    @reviews = Review.all()
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      render 'api/reviews/show'
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
   end
 
   def destroy
   end
-  
+
+  def review_params
+    params.require(:review).permit(:review, :book_id, :user_id)
+  end
 end
---
+++ app/views/api/reviews/_review.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! review, :id,:review, :user_id, :book_id
