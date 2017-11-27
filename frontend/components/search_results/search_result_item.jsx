4ad77727005afdff0801a67e244710493767fc9d
George Najm <georgenajm28@yahoo.com>
2017-11-27 11:21:15 -0500

    added statuses on search item books

--- frontend/components/search_results/search_result_item.jsx
+++ frontend/components/search_results/search_result_item.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Link,withRouter } from 'react-router-dom';
+import BookStatusContainer from '../book_status/book_status_container';
 
 export const SearchResultItem = ({book,review}) => {
-  let stat;
-  if(!review){
-    stat =
-    (
-      <div className="book-status">
-      <button className="status-button">Status</button>
-      <button className="change-status-button">
-        <i className="fa fa-sort-desc" aria-hidden="true"></i>
-      </button>
-    </div>
-    );
-  }
+
   return (
     <div className="result-item">
       <div className="book-hold">
@@ -31,7 +21,9 @@ export const SearchResultItem = ({book,review}) => {
         </div>
       </div>
 
-      {stat ? stat : ""}
+      <div className="book-status">
+        <BookStatusContainer book={book} />
+      </div>
 
     </div>
   );
