fd71d863459774fab694125501f83b17991c2b71
George Najm <georgenajm28@yahoo.com>
2018-01-08 19:04:47 -0500

    added footer to login page

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
   return (
       <div className="footer">
-        HSADHSAUIDHSAUIDSAHUDASIUDHSADIUDAS
+        <div className="copywright">© GreatReads Inc</div>
+        <div className="links">
+          <a className="footerlink" href="https://github.com/georgesco94/greatReads">Github</a>
+          <a className="footerlink" href="https://www.linkedin.com/in/george-najm-707003149/">Linkedin</a>
+          <a className="footerlink" href="https://www.amazon.com/">Amazon</a>
+          <a className="footerlink" href="https://www.thriftbooks.com/">Thriftbooks</a>
+        </div>
       </div>
   );
 };
--- frontend/components/login_page/login_page.jsx
+++ frontend/components/login_page/login_page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SessionFormContainer from '../session_form/session_form_container';
 import NavBar from '../nav_bar/nav_bar';
+import Footer from '../footer/footer';
 import DiscoverBoxContainer from '../discover_box/discover_box_container';
 
 
@@ -12,6 +13,7 @@ const LoginPage = () => (
         <SessionFormContainer />
         <DiscoverBoxContainer />
       </div>
+      <Footer />
     </div>
   </main>
 );
