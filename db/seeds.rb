bb2c9c00287da0cd2aa68da247d9f5fedbcea2c1
George Najm <georgenajm28@yahoo.com>
2018-05-18 23:29:16 -0400

    added multiple genres to all books

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,6 +50,10 @@ short_stories = Genre.new(name:"Short Stories", description: "Short stories")
 short_stories.save!
 classics = Genre.new(name:"Classics", description: "Classics")
 classics.save!
+poetry = Genre.new(name:"Poetry", description: "Poetry")
+poetry.save!
+drama = Genre.new(name:"Drama", description: "Drama")
+drama.save!
 
 
 
@@ -59,9 +63,9 @@ the impact of the Napoleonic era on Tsarist society through the stories of five
 Russian aristocratic families")
 war_peace.image = File.open("app/assets/images/war_peace.jpeg")
 war_peace.save!
-w1 = GenreBookAssignment.create!(book_id: war_peace.id, genre_id:fiction.id)
-w2 = GenreBookAssignment.create!(book_id: war_peace.id, genre_id:historical_fiction.id)
-w3 = GenreBookAssignment.create!(book_id: war_peace.id, genre_id:classics.id)
+GenreBookAssignment.create!(book_id: war_peace.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: war_peace.id, genre_id:historical_fiction.id)
+GenreBookAssignment.create!(book_id: war_peace.id, genre_id:classics.id)
 
 
 
@@ -72,41 +76,66 @@ they left behind transformed by other occupants over
 the years.")
 stories.image = File.open("app/assets/images/updike_short.jpg")
 stories.save!
+GenreBookAssignment.create!(book_id: stories.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: stories.id, genre_id:short_stories.id)
 
 snows = Book.new(title:"Snows of Kilimanjaro", author: "Ernest Hemingway" ,
 description:"The Snows of Kilimanjaro and Other Stories is a collection of short stories by Ernest Hemingway, published in 1961. The title story is considered by some to be the best story Hemingway ever wrote.")
 snows.image = File.open("app/assets/images/snows.jpg")
 snows.save!
+GenreBookAssignment.create!(book_id: snows.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: snows.id, genre_id:short_stories.id)
+GenreBookAssignment.create!(book_id: snows.id, genre_id:classics.id)
+
 
 ezra = Book.new(title:"Selected Poems", author: "Ezra Pound" ,
 description:"Poems by Ezra pound")
 ezra.image = File.open("app/assets/images/ezra.jpg")
 ezra.save!
+GenreBookAssignment.create!(book_id: ezra.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: ezra.id, genre_id:poetry.id)
 
 henry = Book.new(title:"King Henry iv", author: "William Shakespeare" ,
 description:"Henry IV, Part 1 is a history play by William Shakespeare, believed to have been written no later than 1597.")
 henry.image = File.open("app/assets/images/henry.jpg")
 henry.save!
+GenreBookAssignment.create!(book_id: henry.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: henry.id, genre_id:historical_fiction.id)
+GenreBookAssignment.create!(book_id: henry.id, genre_id:classics.id)
+GenreBookAssignment.create!(book_id: henry.id, genre_id:drama.id)
 
 lear = Book.new(title:"King Lear", author: "William Shakespeare" ,
 description:"King Lear is a tragedy written by William Shakespeare. It depicts the gradual descent into madness of the title character, after he disposes of his kingdom giving ")
 lear.image = File.open("app/assets/images/lear.jpg")
 lear.save!
+GenreBookAssignment.create!(book_id: lear.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: lear.id, genre_id:historical_fiction.id)
+GenreBookAssignment.create!(book_id: lear.id, genre_id:classics.id)
+GenreBookAssignment.create!(book_id: lear.id, genre_id:drama.id)
 
 twelfth = Book.new(title:"Twelfth Night", author: "William Shakespeare" ,
 description:"Play by Shakespeare")
 twelfth.image = File.open("app/assets/images/twelfth.jpg")
 twelfth.save!
+GenreBookAssignment.create!(book_id: twelfth.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: twelfth.id, genre_id:historical_fiction.id)
+GenreBookAssignment.create!(book_id: twelfth.id, genre_id:classics.id)
+GenreBookAssignment.create!(book_id: twelfth.id, genre_id:drama.id)
 
 slaughter = Book.new(title:"Slaughterhouse-Five", author: "Kurt Vonnegut Jr." ,
 description:"Kurt Vonnegut's absurdist classic Slaughterhouse-Five introduces us to Billy Pilgrim, a man who becomes unstuck in time ")
 slaughter.image = File.open("app/assets/images/slaughter.jpg")
 slaughter.save!
+GenreBookAssignment.create!(book_id: slaughter.id, genre_id:fiction.id)
+
 
 tale = Book.new(title:"A tale of Two Cities", author: "Charles Dickens" ,
 description:"'Liberty, equality, fraternity, or death; -- the last, much the easiest to bestow, O Guillotine!' After eighteen years as a political prisoner in the Bastille ")
 tale.image = File.open("app/assets/images/tale.jpg")
 tale.save!
+GenreBookAssignment.create!(book_id: henry.id, genre_id:fiction.id)
+GenreBookAssignment.create!(book_id: henry.id, genre_id:historical_fiction.id)
+GenreBookAssignment.create!(book_id: henry.id, genre_id:classics.id)
 
 
 s0 = Status.create!(user_id:user1.id , book_id:slaughter.id ,status:"read")
