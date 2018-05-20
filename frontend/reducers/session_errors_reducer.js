f4e2692a2de66094ba135f979f5439958357530a
George Najm <georgenajm28@yahoo.com>
2018-05-19 22:23:33 -0400

    fixed error remaining bug

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,8 @@
 import merge from 'lodash/merge';
 
 import {RECEIVE_SESSION_ERRORS,RECEIVE_CURRENT_USER} from '../actions/session_actions';
+import {RECEIVE_REVIEWS} from '../actions/review_actions';
+import {RECEIVE_SEARCH_BOOKS} from '../actions/book_actions';
 
 const SessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
@@ -8,6 +10,10 @@ const SessionErrorsReducer = (state = [], action) => {
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
     case RECEIVE_CURRENT_USER:
+      return [];
+    case  RECEIVE_SEARCH_BOOKS:
+      return [];
+    case  RECEIVE_REVIEWS:
       return [];
     default:
       return state;
